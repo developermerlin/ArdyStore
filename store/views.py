336105b20c0f0aaee24674d2eca51815098c7e42
merlin <letscodewithmerlin@gmail.com>
2025-09-07 15:24:27 +0000

    this is another commit

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from carts.models import CartItem
 from carts.views import _cart_id
 from category.models import Category
 from .models import Product
+from django.db.models import Q
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from . import views
 
@@ -47,3 +48,16 @@ def product_detail(request, category_slug, product_slug):
         
     }
     return render(request, 'store/product_detail.html',context)
+
+# search
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
